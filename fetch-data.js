9133add6e5b5c735f09c7d43af1ca5d4b9a6737e
kidistMulualem <mulualemkidist4@gmail.com>
2025-02-06 20:48:17 +0900

    update the content

--- fetch-data.js
+++ fetch-data.js
@@ -1,8 +1,8 @@
 async function fetchUserData(){
-    const apiurl='https://jsonplaceholder.typicode.com/users';
+    const apiUrl='https://jsonplaceholder.typicode.com/users';
     const dataContainer=document.getElementById('api-data');
     try{ 
-        const response=await fetch(apiurl);
+        const response=await fetch(apiUrl);
         const users=await response.json();
         dataContainer.innerHTML=" ";
         const userList=document.createElement('ul');
