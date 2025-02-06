66610f331391401d552d4f35e08ad19ba210fb12
kidistMulualem <mulualemkidist4@gmail.com>
2025-02-06 10:17:54 +0900

    updated

--- script.js
+++ script.js
@@ -10,18 +10,18 @@ document.addEventListener("DOMContentLoaded",function(){
     const email=document.getElementById("email").value.trim();
     const password=document.getElementById("password").value.trim;
     const isValid=true;
-    const massages=[];
+    const messages=[];
     if(username.length<3){
         isValid=false;
-        massages.push("The length should be greaterthan 3.");
+        messages.push("The length should be greaterthan 3.");
     }
     if(!email.includes("@")||!email.includes(".")){
         isValid=false;
-        massages.push('The email should include those characters.');
+        messages.push('The email should include those characters.');
     }
     if (password.length<8){
         isValid=false;
-        massages.push("password should greater than 8");
+        messages.push("password should greater than 8");
     }
     feedbackDiv.style.display="block";
     if(isValid){
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded",function(){
         feedbackDiv.style.color="#28a745";
 
     }else{
-        feedbackDiv.innerHTML= massages.join('br');
+        feedbackDiv.innerHTML= messages.join('br');
        
         feedbackDiv.style.color="dc3545";
     }
